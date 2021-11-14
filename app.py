945d89e58b4cdf2af7520280f258e5eb545203f2
Joel John Joseph <72274851+JoelJJoseph@users.noreply.github.com>
2021-11-14 18:17:20 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import streamlit as st
+import tensorflow as tf
 from multiapp import MultiApp
-import gloww, alz, chest, lung, skin
+
+
 import base64
 from PIL import Image
-
+import keras
+import gloww, alz, chest, lung, skin
 
 from PIL import Image, ImageOps
 import numpy as np
-from img_classification import teachable_machine_classification # import your app modules here
 app = MultiApp()
 
 new_title = '<p style="font-family:sans-serif; color:Black; font-size: 52px;"><b>Cordial Care.AI</b></p>'
@@ -16,7 +18,7 @@ st.markdown(new_title, unsafe_allow_html=True)
 
 # Add all your application here
 app.add_app("Cordial care.AI", gloww.app)
-app.add_app("Alzhemiers", alz.app)
+app.add_app("Alzhemiers Allergy", alz.app)
 app.add_app("Chest cancer", chest.app)
 app.add_app("Lung cancer", lung.app)
 app.add_app("Skin cancer", skin.app)

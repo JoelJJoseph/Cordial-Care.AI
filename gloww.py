76afb210de18c91f12b066c6a33a3b09ccef6467
Joel John Joseph <72274851+JoelJJoseph@users.noreply.github.com>
2021-11-23 21:37:14 +0530

    Update gloww.py

--- gloww.py
+++ gloww.py
@@ -36,3 +36,48 @@ def app():
     st.markdown(new_title, unsafe_allow_html=True)
     
     
+    st.write(" ")
+    result = st.button ("Video Calling")
+    if result:
+       st.components.v1.html(
+"""<head>
+  <script src='https://meet.jit.si/external_api.js'></script>
+</head>
+<body>
+ 
+<button id="start" type="button">Start</button>
+<div id="jitsi-container">
+</div>
+ 
+<script>
+var button = document.querySelector('#start');
+var container = document.querySelector('#jitsi-container');
+var api = null;
+ 
+button.addEventListener('click', () => {
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var stringLength = 30;
+ 
+    function pickRandom() {
+    return possible[Math.floor(Math.random() * possible.length)];
+    }
+ 
+var randomString = Array.apply(null, Array(stringLength)).map(pickRandom).join('');
+ 
+    var domain = "meet.jit.si";
+    var options = {
+        "roomName": randomString,
+        "parentNode": container,
+        "width": 600,
+        "height": 600,
+    };
+    api = new JitsiMeetExternalAPI(domain, options);
+});
+ 
+</script>
+</body>
+</html>"""
+,
+    height=600
+)   
+    
